ba562ffcd73c92d74184103fc886fcad43e56dc5
Andrew Bradford <andrew@bradfordembedded.com>
2016-06-07 11:45:04 -0400

    pru0_spi.c: Whitespace cleanup for indentation and braces
    Clean up the indentation and many brace style issues.  Does not fix all
    whitespace issues such as in assignment statements.
    
    Signed-off-by: Andrew Bradford <andrew@bradfordembedded.com>

--- pru0_spi.c
+++ pru0_spi.c
@@ -1,9 +1,3 @@
-/************************************************************************************
-
-
-
-
-*************************************************************************************/
 #include <stdint.h>
 #include <pru_cfg.h>
 #define   P8_11  15
@@ -12,7 +6,7 @@ volatile register uint32_t __R30;
 //volatile register uint32_t __R31
 int CPOL=0,CPHA=0;
 
- void main()
+void main()
 {
 	volatile uint32_t mosi ,miso;
 	//Set the CFG Register to direct output
@@ -25,81 +19,71 @@ int CPOL=0,CPHA=0;
 	mosi<<=P8_11;
 	//set value of the choice variable between 0 and 3
 	int choice=CPOL*2+CPHA;
-	if(choice>=2)
-	{
+
+	if(choice>=2) {
 		//set the clock 1 to be in its idle state
 		__R30^=(1<<CLK);
 	}
 
-	switch(choice){
-		case 0:	
+	switch(choice) {
+	case 0:
 
-			for (int i=0;i<8;i++)
-			{
-				//set clk 1
-				__R30^=(1<<CLK);
-				//write data to it
-				__R30=mosi| __R30;
-				//set clk 0
-				__R30^=(1<<CLK);
-				//Reset the register
-				__R30=0x0000;
-				//shift data in Mosi
-				mosi<<=1;
+		for (int i=0;i<8;i++) {
+			//set clk 1
+			__R30^=(1<<CLK);
+			//write data to it
+			__R30=mosi| __R30;
+			//set clk 0
+			__R30^=(1<<CLK);
+			//Reset the register
+			__R30=0x0000;
+			//shift data in Mosi
+			mosi<<=1;
+		}
 
-		    }
+	case 1:
+		for (int i=0;i<8;i++) {
+			//set clk 1
+			__R30^=(1<<CLK);
+			//set clk 0
+			__R30^=(1<<CLK);
+			//write data on the falling edge
+			__R30=mosi| __R30;
 
-		case 1:
-			for (int i=0;i<8;i++)
-			{
-				//set clk 1
-				__R30^=(1<<CLK);
-				//set clk 0
-				__R30^=(1<<CLK);
-				//write data on the falling edge
-				__R30=mosi| __R30;
-				
-				//Reset the register
-				__R30=0x0000;
-				//shift data in Mosi
-				mosi<<=1;
+			//Reset the register
+			__R30=0x0000;
+			//shift data in Mosi
+			mosi<<=1;
+		}
 
-		    }
-			                  
-		case 2:
-			for (int i=0;i<8;i++)
-			{
-				//set clk 0 to set it in active state
-				__R30^=(1<<CLK);
-				//write data on the falling edge
-				__R30=mosi| __R30;
-				//set clk 1 to idle state
-				__R30^=(1<<CLK);
-				
-				//Reset the data pin in the register to 0
-				__R30&=(0<<P8_11);
-				//shift data in Mosi
-				mosi<<=1;
+	case 2:
+		for (int i=0;i<8;i++) {
+			//set clk 0 to set it in active state
+			__R30^=(1<<CLK);
+			//write data on the falling edge
+			__R30=mosi| __R30;
+			//set clk 1 to idle state
+			__R30^=(1<<CLK);
 
-		    }
+			//Reset the data pin in the register to 0
+			__R30&=(0<<P8_11);
+			//shift data in Mosi
+			mosi<<=1;
+		}
 
-		case 3:
-			for (int i=0;i<8;i++)
-			{
-				//set clk 0 to set it in active state
-				__R30^=(1<<CLK);
-				//set clk 1 to idle state
-				__R30^=(1<<CLK);
-				//write data on the rising edge
-				__R30=mosi| __R30;
-				//Reset the data pin in the register to 0
-				__R30&=(0<<P8_11);
-				//shift data in Mosi
-				mosi<<=1;
-
-		    }
-
-
+	case 3:
+		for (int i=0;i<8;i++) {
+			//set clk 0 to set it in active state
+			__R30^=(1<<CLK);
+			//set clk 1 to idle state
+			__R30^=(1<<CLK);
+			//write data on the rising edge
+			__R30=mosi| __R30;
+			//Reset the data pin in the register to 0
+			__R30&=(0<<P8_11);
+			//shift data in Mosi
+			mosi<<=1;
+		}
+	}
 }
 
-}
